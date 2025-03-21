326b23c5e9e9dd7ebcb5e8e58461f95eddea60b0
rohit <rohit@outworldconcepts.com>
2025-03-21 21:02:10 +0530

    Setup initial structure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-export default {};
+type Event = {};
+
+type SendFn = (e: Event) => {};
+
+type Workflow = {};
+
+type EventData = {};
+
+type Step = {};
+
+type WorkflowInput = {
+  e: EventData;
+  s: Step;
+};
+
+type CreateWorkFlowFn = (input: WorkflowInput) => Workflow;
+
+type Durable = {
+  send: SendFn;
+  createWorkFlow: CreateWorkFlowFn;
+};
+
+const durable = () => {};
+
+const createWorkFlow = (input: WorkflowInput): Workflow => {
+  return {} satisfies Workflow;
+};
+
+export default {
+  durable,
+};
